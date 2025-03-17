0d43364730191422eeba55da7e1bbbc396bfd56f
Vertfle <161231677+Vertfle@users.noreply.github.com>
2025-03-17 16:02:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot('7898320721:AAHUS4O-bUMdn4JNT21OTPi4t3oXvBtB1Dk')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.username}. '
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}. '
                                       f'Напишите во сколько часов вы хотите спать:')
 
 bot.infinity_polling()
